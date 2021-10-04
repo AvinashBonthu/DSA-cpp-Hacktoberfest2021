fb6f351fc57af7b554706db90fb6c5da39f2cd42
saisyama paruchuru <47055971+saisyama31@users.noreply.github.com>
2021-10-04 12:51:13 +0530

    Update 5. Maximum number of 2*2 squares .cpp

--- 02. Mathematics/5. Maximum number of 2*2 squares .cpp
+++ 02. Mathematics/5. Maximum number of 2*2 squares .cpp
@@ -1,70 +1,25 @@
-/**
-5. Maximum number of 2*2 squares 
-Given the base (in units) of a right-angled isoceles traingle, find the maximum number of 2×2 squares that can fit in the triangle with given base.
-
-Example 1:
-
-Input: 8
-Output: 6
-Explanation:
-Please refer below diagram for explanation.
-squares-in-triangle
-
-Example 2:
-
-Input : 7
-Output : 3
- 
-Your task :
-You don't need to read input or print anything. Your task is to complete the function numberOfSquares() which takes the base of the triangle as input and returns the maximum number of squares that can be fit into the triangle with given base.
- 
-Expected Time Complexity: O(1)
-Expected Auxiliary Space: O(1)
- 
-Constraints :
-1 <= N <= 10^9
-**/
-// { Driver Code Starts
-//Initial Template for C++
-
+// C++ program to count number of 2 x 2
+// squares in a right isosceles triangle
 #include<bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
-//User function Template for C++
-
-class Solution
+int numberOfSquares(int base)
 {
-    public:
-    long long int numberOfSquares(long long int base)
-    {
-        //code here
-        if (base%2 == 0)
-        return (base)*(base-2)/8;
-        else
-        return(base -3)*(base-1)/8;
-        
-    }
-};
+// removing the extra part we would
+// always need
+base = (base - 2);
 
-// { Driver Code Starts.
+// Since each square has base of
+// length of 2
+base = floor(base / 2);
 
+return base * (base + 1)/2;
+}
+
+// Driver code
 int main()
 {
-	int t;
-	cin >> t;
-
-	while (t--)
-	{
-		long long int base;
-		cin >> base;
-
-        Solution ob;
-		cout << ob.numberOfSquares(base) << "\n";
-
-	}
-
-	return 0;
+int base = 8;
+cout << numberOfSquares(base);
+return 0;
 }
-  // } Driver Code Ends
